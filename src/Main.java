8ff48b4b8ff5e790d8eca76c2196c01254bc513d
MamatisakovDaniyar <mamatisakovdaniar006@gmail.com>
2024-05-03 16:01:08 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String mycountry;
+         final int place =18;
+         String word;
+         System.out.println("mycuntry "+ 18 +" word");
+
+    }
+}
