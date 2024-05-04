ea627ff93ae3acb9157619b2c7c0e8143e128ae8
MamatisakovDaniyar <mamatisakovdaniar006@gmail.com>
2024-05-04 08:52:45 +0600

    String myCity;
            final int NUM = 1;
    
            String word = "Bishkek";
    
            myCity = NUM + word;
            System.out.println("Значения mycity " + myCity);
            System.out.println("Значения NUM " + NUM );
            System.out.println("Значения word " + word );
    
            if (NUM<0 ){
                System.out.println(" Вы сохранили отрицательное число");
            }else if (NUM>0){
                System.out.println(" Вы сохранили положительное число");
            } else {
                System.out.println(" Вы сохранили ноль");
            }
    
            System.out.print(" Введите ваше имя");
    
            Scanner scanner = new Scanner(System.in);
            String myname = scanner.nextLine();
    
            System.out.println(" Привет " + myname + "!");
    
            scanner.close();

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String mycountry;
-         final int place =18;
-         String word;
-         System.out.println("mycuntry "+ 18 +" word");
+        String myCity;
+
+        final int NUM = 1;
+
+        String word = "Bishkek";
+
+
+        myCity = NUM + word;
+        System.out.println("Значения mycity " + myCity);
+        System.out.println("Значения NUM " + NUM );
+        System.out.println("Значения word " + word );
+
+        if (NUM<0 ){
+            System.out.println(" Вы сохранили отрицательное число");
+        }else if (NUM>0){
+            System.out.println(" Вы сохранили положительное число");
+        } else {
+            System.out.println(" Вы сохранили ноль");
+        }
+
+
+        System.out.print(" Введите ваше имя");
+
+
+        Scanner scanner = new Scanner(System.in);
+        String myname = scanner.nextLine();
+
+
+        System.out.println(" Привет " + myname + "!");
+
+        scanner.close();
 
     }
 }
